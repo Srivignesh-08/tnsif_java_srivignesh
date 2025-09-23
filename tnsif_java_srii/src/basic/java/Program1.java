3dd8d9e384fe53c213e950830f50088ac3450ca9
sri14 <sri14@SRII>
2025-09-23 15:29:36 +0530

    commit2

--- tnsif_java_srii/src/basic/java/Program1.java
+++ tnsif_java_srii/src/basic/java/Program1.java
@@ -1,10 +1,14 @@
+//Program to demonstrate  break statement
 package basic.java;
 
 public class Program1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
 
 	}
-
 }
